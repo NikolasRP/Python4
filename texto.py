c495b5b273cf1d0cecc649a9eb7bff77ecbccab7
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-11 10:07:53 -0300

    Update texto.py

--- texto.py
+++ texto.py
@@ -8,5 +8,5 @@ class MainApp(App):
     label = Label (text = "fala galera!", size_hint = (5, .5), pos_hint = {"center_x": .5, "center_y": .5})
     return label
     if __name__ == "__main__":
-      app = MainApp()
+      app = MainApp(App)
       app.run
